2faa3fe7ef3c83a2464465c8c2fd5023016f4407
Dusan Velimirovic <89640144+DusanVelimirovic@users.noreply.github.com>
2024-06-07 09:54:28 +0200

    Update ConversionManager.cs
    Code analysis

--- Conversions/ConversionManager.cs
+++ Conversions/ConversionManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+// Strategy object reference to Strategy interface and responsible for implementing different strategies.
+using System.Collections.Generic;
 using System.Linq;
 
 namespace ConverterApp.Conversions
@@ -7,11 +8,13 @@ namespace ConverterApp.Conversions
     {
         private readonly List<IConversion> _conversions = new List<IConversion>();
 
+        // Adds a new conversion strategy
         public void RegisterConversion(IConversion conversion)
         {
             _conversions.Add(conversion);
         }
 
+        // Retrieves a conversion strategy based on its unique identifier
         public IConversion GetConversion(int id)
         {
             return _conversions.FirstOrDefault(c => c.Id == id);
